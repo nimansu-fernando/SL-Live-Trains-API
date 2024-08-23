874425ec8962eee301d0ee07ff9adfaf15e2d829
nimansu-fernando <nimansufernando@gmail.com>
2024-08-23 13:38:28 +0530

    Add route model

---
+++ services/railway-infrastructure-service/models/station-model.js
@@ -0,0 +1,29 @@
+const db = require('../config/db-config');
+
+const getAllStations = () => {
+    return db.query('SELECT * FROM stations');
+};
+
+const getStationById = (id) => {
+    return db.query('SELECT * FROM stations WHERE id = ?', [id]);
+};
+
+const createStation = (station) => {
+    return db.query('INSERT INTO stations SET ?', station);
+};
+
+const updateStation = (id, station) => {
+    return db.query('UPDATE stations SET ? WHERE id = ?', [station, id]);
+};
+
+const deleteStation = (id) => {
+    return db.query('DELETE FROM stations WHERE id = ?', [id]);
+};
+
+module.exports = {
+    getAllStations,
+    getStationById,
+    createStation,
+    updateStation,
+    deleteStation
+};
