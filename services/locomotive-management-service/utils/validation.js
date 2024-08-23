a10fa411edeba273ee1ad3068e73ddf5e83ccce8
nimansu-fernando <nimansufernando@gmail.com>
2024-08-23 15:00:13 +0530

    Add validations for engines and train data in Locomotive Management Service

---
+++ services/locomotive-management-service/utils/validation.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const engineSchema = Joi.object({
+    engine_number: Joi.string().max(255).required(),
+    type: Joi.string().max(255).required(),
+    class: Joi.string().max(255).required(),
+    capacity: Joi.number().integer().required(),  
+    power: Joi.number().integer().required(),  
+    status: Joi.string().max(255).required()  
+});
+
+const trainSchema = Joi.object({
+    train_number: Joi.string().max(255).required(),
+    name: Joi.string().max(255).required(),
+    number_of_coaches: Joi.number().integer().required(),  
+    seating_capacity: Joi.number().integer().required(),  
+    class_configuration: Joi.string().max(255).required(), 
+    status: Joi.string().max(255).required(),  
+    engine_number: Joi.string().max(255).allow(null)  
+});
+
+module.exports = {
+    engineSchema,
+    trainSchema
+};
