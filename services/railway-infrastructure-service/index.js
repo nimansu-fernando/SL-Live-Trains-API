60b87fb63f6662e3933629a323b9b104b5372a6e
nimansu-fernando <nimansufernando@gmail.com>
2024-08-23 13:50:51 +0530

    Setup Express server and configure routes for Railway Infrastructure Service

---
+++ services/railway-infrastructure-service/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const lineRoutes = require('./routes/line-routes');
+const routeRoutes = require('./routes/route-routes');
+const stationRoutes = require('./routes/station-routes');
+require('dotenv').config();
+
+app.use(express.json());
+app.use('/lines', lineRoutes);
+app.use('/routes', routeRoutes);
+app.use('/stations', stationRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Railway Infrastructure Service running on port ${PORT}`);
+});
