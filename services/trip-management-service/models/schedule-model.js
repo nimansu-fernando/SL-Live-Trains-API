3f0bfe0cf4abcdcb051558c0eb1314c138102139
nimansu-fernando <nimansufernando@gmail.com>
2024-08-23 15:13:04 +0530

    Add schedule model to Trip Management Service

---
+++ services/trip-management-service/models/schedule-model.js
@@ -0,0 +1,29 @@
+const db = require('../config/db-config');
+
+const getAllSchedules = () => {
+    return db.query('SELECT * FROM schedules');
+};
+
+const getScheduleById = (id) => {
+    return db.query('SELECT * FROM schedules WHERE id = ?', [id]);
+};
+
+const createSchedule = (schedule) => {
+    return db.query('INSERT INTO schedules SET ?', schedule);
+};
+
+const updateSchedule = (id, schedule) => {
+    return db.query('UPDATE schedules SET ? WHERE id = ?', [schedule, id]);
+};
+
+const deleteSchedule = (id) => {
+    return db.query('DELETE FROM schedules WHERE id = ?', [id]);
+};
+
+module.exports = {
+    getAllSchedules,
+    getScheduleById,
+    createSchedule,
+    updateSchedule,
+    deleteSchedule
+};
