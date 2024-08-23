76f2fdc3aa7892902683bfc0f35c61d66d778c5e
nimansu-fernando <nimansufernando@gmail.com>
2024-08-23 13:39:07 +0530

    Add route model

---
+++ services/railway-infrastructure-service/models/route-model.js
@@ -0,0 +1,29 @@
+const db = require('../config/db-config');
+
+const getAllRoutes = () => {
+    return db.query('SELECT * FROM routes');
+};
+
+const getRouteById = (id) => {
+    return db.query('SELECT * FROM routes WHERE id = ?', [id]);
+};
+
+const createRoute = (route) => {
+    return db.query('INSERT INTO routes SET ?', route);
+};
+
+const updateRoute = (id, route) => {
+    return db.query('UPDATE routes SET ? WHERE id = ?', [route, id]);
+};
+
+const deleteRoute = (id) => {
+    return db.query('DELETE FROM routes WHERE id = ?', [id]);
+};
+
+module.exports = {
+    getAllRoutes,
+    getRouteById,
+    createRoute,
+    updateRoute,
+    deleteRoute
+};
