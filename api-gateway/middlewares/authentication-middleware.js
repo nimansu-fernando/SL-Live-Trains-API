6b437d8978e55606a1c66c2026669bd8d5303cc8
nimansu-fernando <nimansufernando@gmail.com>
2024-08-25 04:25:38 +0530

    Update authentication middleware

--- api-gateway/middlewares/authentication-middleware.js
+++ api-gateway/middlewares/authentication-middleware.js
@@ -2,16 +2,16 @@ const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
     const token = req.headers['authorization'];
-    
+
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
-    
+
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: 'Failed to authenticate token' });
         }
-        
+
         req.user = decoded;
         next();
     });
