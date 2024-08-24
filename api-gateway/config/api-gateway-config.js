ed9097158d5083052419b01ca5bc333c0c9208f6
nimansu-fernando <nimansufernando@gmail.com>
2024-08-24 20:17:50 +0530

    Add API gateway configuration

---
+++ api-gateway/config/api-gateway-config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const config = {
+    PORT: process.env.PORT || 3000,
+    SERVICE_URLS: {
+        RAILWAY_INFRASTRUCTURE: process.env.RAILWAY_INFRASTRUCTURE_URL || 'http://localhost:3001',
+        LOCOMOTIVE_MANAGEMENT: process.env.LOCOMOTIVE_MANAGEMENT_URL || 'http://localhost:3002',
+        TRIP_MANAGEMENT: process.env.TRIP_MANAGEMENT_URL || 'http://localhost:3003',
+        LOCATION: process.env.LOCATION_URL || 'http://localhost:3004',
+        DATA_INGESTION: process.env.DATA_INGESTION_URL || 'http://localhost:3005'
+    }
+};
+
+module.exports = config;
