070442e7dc03a495e070abf59c37b2a0128aa3a7
nimansu-fernando <nimansufernando@gmail.com>
2024-08-24 22:23:46 +0530

    Route changes

--- services/location-service/routes/location-routes.js
+++ services/location-service/routes/location-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const locationController = require('../controllers/location-controller');
 
-router.post('/location', locationController.postLocationData);
-router.get('/locations', locationController.getAllLocations);
+router.post('/', locationController.postLocationData);
+router.get('/data', locationController.getAllLocations);
 
 module.exports = router;
