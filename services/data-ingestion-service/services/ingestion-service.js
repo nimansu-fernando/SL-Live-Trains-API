9c54e2c798b521931d89e8c6a3694d627d152a7c
nimansu-fernando <nimansufernando@gmail.com>
2024-08-24 15:54:27 +0530

    Add ingestion service to Data Ingestion Service

---
+++ services/data-ingestion-service/services/ingestion-service.js
@@ -0,0 +1,33 @@
+const IngestionModel = require('../models/ingestion-model');
+const axios = require('axios');
+
+const LOCATION_SERVICE_URL = 'http://localhost:3000/location/api/location';
+
+async function ingestData(data) {
+    try {
+        if (!data.deviceID || !data.latitude || !data.longitude || !data.timestamp) {
+            console.error('Invalid data received:', data);
+            return;
+        }
+
+        const ingestedData = new IngestionModel(data);
+        await ingestedData.save();
+
+        const geoLocation = await reverseGeocode(data.latitude, data.longitude);
+
+        const enrichedData = { ...data, geoLocation };
+
+        console.log('Processed data with location:', enrichedData);
+
+        await axios.post(LOCATION_SERVICE_URL, enrichedData);
+
+        return enrichedData;
+    } catch (error) {
+        console.error('Failed to process and store data:', error.message);
+        throw new Error('Failed to process data');
+    }
+}
+
+module.exports = {
+    ingestData,
+};
