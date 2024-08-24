68142bd504cf47fb4ab914389a4222d38e95c4ff
nimansu-fernando <nimansufernando@gmail.com>
2024-08-24 15:55:47 +0530

    Reverse GeoCoading

--- services/data-ingestion-service/services/ingestion-service.js
+++ services/data-ingestion-service/services/ingestion-service.js
@@ -3,6 +3,28 @@ const axios = require('axios');
 
 const LOCATION_SERVICE_URL = 'http://localhost:3000/location/api/location';
 
+async function reverseGeocode(latitude, longitude) {
+    const nominatimUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&accept-language=en`;
+    
+    try {
+        const response = await axios.get(nominatimUrl);
+        const data = response.data;
+
+        let geoLocation;
+
+        if (data.address && (data.address.railway || data.name) && (data.addresstype === 'railway' || data.class === 'railway' || data.type === 'station')) {
+            geoLocation = data.address.railway || data.name;
+        } else {
+            geoLocation = data.address.suburb || data.address.village || data.name || 'Heading to next station';
+        }
+
+        return geoLocation;
+    } catch (error) {
+        console.error('Error in reverse geocoding:', error.message);
+        return 'Unknown Location';
+    }
+}
+
 async function ingestData(data) {
     try {
         if (!data.deviceID || !data.latitude || !data.longitude || !data.timestamp) {
