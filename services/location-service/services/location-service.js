724fd8487c6f1b857ba29549b49bdaa18c66737f
nimansu-fernando <nimansufernando@gmail.com>
2024-08-24 19:51:57 +0530

    Add location service to Location Service

--- services/location-service/services/location-service.js
+++ services/location-service/services/location-service.js
@@ -2,25 +2,20 @@ const LocationModel = require('../models/location-model');
 
 async function storeOrUpdateLocation(data) {
     try {
-        // Find an existing location by deviceID
         const existingLocation = await LocationModel.findOne({ deviceID: data.deviceID });
 
         if (existingLocation) {
-            // Update only specific fields
             existingLocation.latitude = data.latitude;
             existingLocation.longitude = data.longitude;
             existingLocation.speed = data.speed;
             existingLocation.timestamp = data.timestamp;
             existingLocation.geoLocation = data.geoLocation;
-            // Only update these fields for existing data
         } else {
-            // Insert new data with all fields
             const newLocation = new LocationModel(data);
-            await newLocation.save();  // Save the new document
             return newLocation;
         }
 
-        await existingLocation.save();  // Save the updated document
+        await existingLocation.save();  
         return existingLocation;
     } catch (error) {
         throw new Error('Failed to store or update location');
@@ -29,7 +24,6 @@ async function storeOrUpdateLocation(data) {
 
 async function getAllActiveLocations() {
     try {
-        // Fetch all location documents from the database
         const locations = await LocationModel.find({});
         return locations;
     } catch (error) {
