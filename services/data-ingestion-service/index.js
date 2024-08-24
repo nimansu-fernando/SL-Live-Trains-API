67b76bba3ecee97ad57f54f3b1085aae5714d4ed
nimansu-fernando <nimansufernando@gmail.com>
2024-08-24 15:54:54 +0530

    Setup Express server and configure routes for Data Ingestion Service

---
+++ services/data-ingestion-service/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db-config');
+const ingestionRoutes = require('./routes/ingestion-routes');
+require('dotenv').config();
+
+const app = express();
+
+connectDB();
+app.use(express.json());
+
+app.use('/api/ingestion', ingestionRoutes);
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+  console.log(`Data Ingestion Service running on port ${PORT}`);
+});
