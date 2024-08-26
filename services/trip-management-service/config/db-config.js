e0ac0905028b7a4460e752d9be4d72a2faed9317
nimansu-fernando <nimansufernando@gmail.com>
2024-08-26 23:51:47 +0530

    Update database configuration to include port from environment variable

--- services/trip-management-service/config/db-config.js
+++ services/trip-management-service/config/db-config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    //connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
